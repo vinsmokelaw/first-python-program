fc4ba9def03051c8824ce10b3a0fca9e38ca36c3
vinsmokelaw <muchenutawanda4@gmail.com>
2025-04-03 12:27:49 +0200

    gear 1

--- omega2.py
+++ omega2.py
@@ -8,7 +8,20 @@ forever = True
 
 while forever:
     number1 = float(input("Enter number: "))
-    apply = input("+, -, *, / ")
-    number2 = float(input("Enter number:  "))
+    operator = input("+, -, *, / ")
+    number2 = float(input("Enter number: "))
+
+    if operator == "+":
+        print(number1 + number2)
+    elif operator == "-":
+        print(number1 - number2)
+    elif operator == "*":
+        print(number1 * number2)
+    elif operator == "/":
+        print(number1 / number2)
+    else:
+        print("wrong_operator use '+', '-', '*', '/',")
+
+
 
           
